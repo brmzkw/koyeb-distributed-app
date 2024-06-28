22cba841b1adbb76bfbacd528ec4718106991302
Julien Castets <castets.j@gmail.com>
2024-06-28 08:56:51 +0200

    Log creds

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ```
 koyeb app create distributed-app
 koyeb service create distributed-app/rabbitmq --docker rabbitmq:management --port 15672 --env RABBITMQ_DEFAULT_USER=xxx --env RABBITMQ_DEFAULT_PASS=xxx
+
+koyeb service create distributed-app/api --git github.com/brmzkw/koyeb-distributed-app --port 8000 --env RABBITMQ_HOST=rabbitmq.distributed-app.koyeb
+
+koyeb service create distributed-app/worker --git github.com/brmzkw/koyeb-distributed-app --env RABBITMQ_HOST=rabbitmq.distributed-app.koyeb --git-buildpack-run-command 'celery -A worker worker --loglevel=INFO' --type worker
 ```
--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import random
 from flask import Flask
+import pika
 
 from worker import celery_app, process_file, RABBITMQ_PASSWORD, RABBITMQ_USER, RABBITMQ_HOST
 
@@ -8,6 +9,12 @@ flask_app = Flask(__name__)
 
 @flask_app.route('/')
 def root():
+    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, credentials=credentials))
+    channel = connection.channel()
+    queue = channel.queue_declare(queue='celery', passive=True)
+    ready_tasks = queue.method.message_count
+
     n = random.randrange(1, 10)
     process_file.delay(n)
     return f'Sent task to worker which will take {n} seconds to complete!'
