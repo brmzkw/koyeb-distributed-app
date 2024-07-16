97b9a5dfbde2c80cdccd953ce4a9fd56854c115c
Julien Castets <castets.j@gmail.com>
2024-07-16 18:12:36 +0200

    wip

--- api.py
+++ api.py
@@ -1,10 +1,97 @@
+import atexit
+import datetime
+import math
+import os
 import random
-from flask import Flask
 
-from worker import celery_app, process_file, RABBITMQ_PASSWORD, RABBITMQ_USER, RABBITMQ_HOST
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask
+import pika
+
+from sdk.openapi_client import ApiClient, Configuration, ServicesApi, AppsApi, DeploymentsApi, UpdateService
+
+from worker import process_file, RABBITMQ_PASSWORD, RABBITMQ_USER, RABBITMQ_HOST
+
 
 
 flask_app = Flask(__name__)
+
+KOYEB_API_KEY = os.environ.get('KOYEB_API_KEY')
+KOYEB_WORKER_APP = os.getenv('KOYEB_WORKER_APP', 'dapp-worker')
+KOYEB_WORKER_SERVICE = os.getenv('KOYEB_WORKER_SERVICE', 'worker')
+
+last_scale_event = datetime.datetime.now()
+
+def scale_app():
+    last_scale = datetime.datetime.now() - last_scale_event
+    if last_scale < datetime.timedelta(minutes=5):
+        print('We scaled the service recently, avoid to scale again')
+        return
+    
+    print('Checking if we need to scale the service...')
+
+    # Connect to RabbitMQ, and get the number of tasks in the default "celery" queue used to process tasks.
+    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, credentials=credentials))
+    channel = connection.channel()
+    queue = channel.queue_declare(queue='celery', passive=True)
+    ready_tasks = queue.method.message_count
+
+    # Let's assume we want to have maximum 10 tasks in the queue per worker, with a minimum of 1 worker
+    expected_count = max(math.ceil(ready_tasks / 10), 1)
+    print(f'>> Queue has {ready_tasks} tasks, expected workers count: {expected_count}')
+
+    # Perform the scaling if needed
+    configuration = Configuration(
+        host="https://app.koyeb.com",
+        api_key={
+            'Bearer': KOYEB_API_KEY,
+        },
+        api_key_prefix={
+            'Bearer': 'Bearer'
+        }
+    )
+
+    with ApiClient(configuration) as api_client:
+        # Retrieve Koyeb application
+        apps = AppsApi(api_client).list_apps(name=KOYEB_WORKER_APP).apps
+        if len(apps) != 1:
+            raise ValueError(f'Expected 1 app with name {KOYEB_WORKER_APP}, got {len(apps)}')
+
+        # Retrieve Koyeb service
+        services = ServicesApi(api_client).list_services(app_id=apps[0].id, name=KOYEB_WORKER_SERVICE).services
+        if len(services) != 1:
+            raise ValueError(f'Expected 1 service with name {KOYEB_WORKER_SERVICE}, got {len(services)}')
+
+        # Get the current deployment
+        latest_deployment_id = services[0].latest_deployment_id
+        deployment = DeploymentsApi(api_client).get_deployment(id=latest_deployment_id).deployment
+
+        # Here, we assume the service is deployed with the same scaling policy everywhere
+        current_count = deployment.definition.scalings[0].min
+
+        if current_count == expected_count:
+            print(f'>> Service already scaled to {expected_count} instances, skipping')
+            return
+        elif current_count > expected_count:
+            direction = 'down'
+            new_count = current_count - 1
+        else:
+            direction = 'up'
+            new_count = current_count + 1
+
+        for idx, _ in enumerate(deployment.definition.scalings):
+            deployment.definition.scalings[idx].min = new_count
+            deployment.definition.scalings[idx].max = new_count
+
+        update_body = UpdateService(definition=deployment.definition, skip_build=True)
+        ServicesApi(api_client=api_client).update_service(
+            id=services[0].id,
+            service=update_body
+        )
+        print(f'Scaling {direction} service to {new_count} instances')
+
+
 
 @flask_app.route('/')
 def root():
@@ -13,5 +100,17 @@ def root():
     return f'Sent task to worker which will take {n} seconds to complete!'
 
 
-if __name__ == "__main__":
+def main():
+    # In debug mode, Flask will reload itself on code changes. Avoid setting up the scheduler in this case.
+    if not flask_app.debug or os.environ.get("WERKZEUG_RUN_MAIN"):
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(func=scale_app, trigger="interval", seconds=1)
+        scheduler.start()
+        # Shut down the scheduler when exiting the app
+        atexit.register(lambda: scheduler.shutdown())
+
     flask_app.run()
+
+
+if __name__ == "__main__":
+    main()
--- worker.py
+++ worker.py
@@ -10,25 +10,11 @@ from sdk.openapi_client import ApiClient, Configuration, ServicesApi, AppsApi, D
 RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'xxx')
 RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASS', 'xxx')
 RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
-KOYEB_API_KEY = os.environ.get('KOYEB_API_KEY')
 
-KOYEB_WORKER_APP = os.getenv('KOYEB_WORKER_APP', 'dapp-worker')
-KOYEB_WORKER_SERVICE = os.getenv('KOYEB_WORKER_SERVICE', 'worker')
 
 celery_app = Celery('tasks', broker=f'pyamqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}//')
-
 celery_app.conf.task_acks_late = True
 celery_app.conf.worker_prefetch_multiplier = 1
-
-celery_app.conf.task_queues = [
-    Queue('high_priority', Exchange('high_priority'), routing_key='high_priority', queue_arguments={'x-max-priority': 10}),
-]
-
-# Use the high_priority queue for the scale_koyeb_service task. If the main
-# queue is overloaded, the scale_koyeb_service should still be able to run.
-celery_app.conf.task_routes = ([
-    ('worker.scale_koyeb_service', {'queue': 'high_priority'}),
-],)
 
 
 @celery_app.task(priority=1)
@@ -38,81 +24,81 @@ def process_file(t):
     print('Task completed!')
 
 
-@celery_app.task(priority=0)
-def scale_koyeb_service():
-    print('Checking if we need to scale the service')
+# @celery_app.task(priority=0)
+# def scale_koyeb_service():
+#     print('Checking if we need to scale the service')
 
-    # Connect to RabbitMQ, and get the number of tasks in the default "celery" queue used to process tasks.
-    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, credentials=credentials))
-    channel = connection.channel()
-    queue = channel.queue_declare(queue='celery', passive=True)
-    ready_tasks = queue.method.message_count
+#     # Connect to RabbitMQ, and get the number of tasks in the default "celery" queue used to process tasks.
+#     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+#     connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, credentials=credentials))
+#     channel = connection.channel()
+#     queue = channel.queue_declare(queue='celery', passive=True)
+#     ready_tasks = queue.method.message_count
 
-    # Let's assume we want to have maximum 10 tasks in the queue per worker
-    expected_count = math.ceil(ready_tasks / 10)
-    print(f'Queue has {ready_tasks} tasks, expected workers count: {expected_count}')
+#     # Let's assume we want to have maximum 10 tasks in the queue per worker
+#     expected_count = math.ceil(ready_tasks / 10)
+#     print(f'Queue has {ready_tasks} tasks, expected workers count: {expected_count}')
 
-    # Perform the scaling if needed
-    do_service_scale(expected_count)
-
-
-def do_service_scale(expected_count):
-    configuration = Configuration(
-        host="https://app.koyeb.com",
-        api_key={
-            'Bearer': KOYEB_API_KEY,
-        },
-        api_key_prefix={
-            'Bearer': 'Bearer'
-        }
-    )
-
-    with ApiClient(configuration) as api_client:
-        # Retrieve Koyeb application
-        apps = AppsApi(api_client).list_apps(name=KOYEB_WORKER_APP).apps
-        if len(apps) != 1:
-            raise ValueError(f'Expected 1 app with name {KOYEB_WORKER_APP}, got {len(apps)}')
-
-        # Retrieve Koyeb service
-        services = ServicesApi(api_client).list_services(app_id=apps[0].id, name=KOYEB_WORKER_SERVICE).services
-        if len(services) != 1:
-            raise ValueError(f'Expected 1 service with name {KOYEB_WORKER_SERVICE}, got {len(services)}')
-
-        # Get the current deployment
-        latest_deployment_id = services[0].latest_deployment_id
-        deployment = DeploymentsApi(api_client).get_deployment(id=latest_deployment_id).deployment
-
-        # Assuming we have only one scaling group, and all the scaling groups have the same min/max values
-        current_count = deployment.definition.scalings[0].min
-
-        if current_count == expected_count:
-            print(f'Service already scaled to {expected_count} instances, skipping')
-            return
-        elif current_count > expected_count:
-            direction = 'down'
-            new_count = current_count - 1
-        else:
-            direction = 'up'
-            new_count = current_count + 1
-
-        # Force to have at least 1 worker
-        if new_count < 1:
-            new_count = 1
-
-        for idx, _ in enumerate(deployment.definition.scalings):
-            deployment.definition.scalings[idx].min = new_count
-            deployment.definition.scalings[idx].max = new_count
-
-        update_body = UpdateService(definition=deployment.definition, skip_build=True)
-        ServicesApi(api_client=api_client).update_service(
-            id=services[0].id,
-            service=update_body
-        )
-        print(f'Scaling {direction} service to {new_count} instances')
+#     # Perform the scaling if needed
+#     do_service_scale(expected_count)
 
 
-@celery_app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    # Executes every 10 seconds
-    sender.add_periodic_task(60.0, scale_koyeb_service.s(), name='Scale Koyeb service')
+# def do_service_scale(expected_count):
+#     configuration = Configuration(
+#         host="https://app.koyeb.com",
+#         api_key={
+#             'Bearer': KOYEB_API_KEY,
+#         },
+#         api_key_prefix={
+#             'Bearer': 'Bearer'
+#         }
+#     )
+
+#     with ApiClient(configuration) as api_client:
+#         # Retrieve Koyeb application
+#         apps = AppsApi(api_client).list_apps(name=KOYEB_WORKER_APP).apps
+#         if len(apps) != 1:
+#             raise ValueError(f'Expected 1 app with name {KOYEB_WORKER_APP}, got {len(apps)}')
+
+#         # Retrieve Koyeb service
+#         services = ServicesApi(api_client).list_services(app_id=apps[0].id, name=KOYEB_WORKER_SERVICE).services
+#         if len(services) != 1:
+#             raise ValueError(f'Expected 1 service with name {KOYEB_WORKER_SERVICE}, got {len(services)}')
+
+#         # Get the current deployment
+#         latest_deployment_id = services[0].latest_deployment_id
+#         deployment = DeploymentsApi(api_client).get_deployment(id=latest_deployment_id).deployment
+
+#         # Assuming we have only one scaling group, and all the scaling groups have the same min/max values
+#         current_count = deployment.definition.scalings[0].min
+
+#         if current_count == expected_count:
+#             print(f'Service already scaled to {expected_count} instances, skipping')
+#             return
+#         elif current_count > expected_count:
+#             direction = 'down'
+#             new_count = current_count - 1
+#         else:
+#             direction = 'up'
+#             new_count = current_count + 1
+
+#         # Force to have at least 1 worker
+#         if new_count < 1:
+#             new_count = 1
+
+#         for idx, _ in enumerate(deployment.definition.scalings):
+#             deployment.definition.scalings[idx].min = new_count
+#             deployment.definition.scalings[idx].max = new_count
+
+#         update_body = UpdateService(definition=deployment.definition, skip_build=True)
+#         ServicesApi(api_client=api_client).update_service(
+#             id=services[0].id,
+#             service=update_body
+#         )
+#         print(f'Scaling {direction} service to {new_count} instances')
+
+
+# @celery_app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Executes every 10 seconds
+#     sender.add_periodic_task(60.0, scale_koyeb_service.s(), name='Scale Koyeb service')
